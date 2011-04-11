5a087a5b2d76ad66061d5fc6632ba01da7372fa3
Radoslav Georgiev <g2h.horde@gmail.com>
2011-04-11 21:22:21 +0300

    Changed the calls to the new functions, also removed yepnope and added jQuery by link

--- index.php
+++ index.php
@@ -9,29 +9,20 @@ $students = array();
 if (isset($_GET["lecture"]) && !empty($_GET["lecture"])) {
 
     $getLecture = $_GET["lecture"];
-    $students = $game->getStudentsByLecture($getLecture);
+    $students = $game->leaderboard("lecture", $getLecture);
     //var_dump($students);
 } else {
-    $students = $game->getStudents();
+    $students = $game->leaderboard();
 }
-// sort for assoc array
-usort($students, array("Student", "compare"));
 ?>
 <htmL>
     <head>
         <title>PHP 11 Course Leaderboard</title>
         <link rel="stylesheet" href="styles/main.css" type="text/css" media="screen" />
-        <script src="javascript/yepnope/yepnope.js" type="text/javascript" language="javascript" charset="utf-8"></script>
         <script src="javascript/namespace.js" type="text/javascript" language="javascript" charset="utf-8"></script>
-        <script src="javascript/jquery-1.5.1.min.js" type="text/javascript" language="javascript" charset="utf-8"></script>
+        <script src="http://code.jquery.com/jquery-1.5.2.js" type="text/javascript" language="javascript" charset="utf-8"></script>
 
         <script type="text/javascript" language="javascript">
-                        // TODO - BARZANA ZA MONITORITE
-                        // DOMASHNO PO DVOIKI, POCHVA OT 3.5, STEP 0.1 --
-                        // DA UPDATE-NA LEADERBOARD-A
-                        // GRUPIRANE PO SPECIALNOST
-                        // interesni vyprosi sa su6to i tezi za interview
-			// http://code.jquery.com/jquery-1.5.2.js			
 			$(document).ready(function() {
 				console.log("asdasdads");
 				$("tr:odd").addClass("odd");
