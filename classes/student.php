eed684913373cea1daa6f8ea4adbc36242745a85
Radoslav Georgiev <g2h.horde@gmail.com>
2011-04-16 16:09:54 +0300

    Added few PHPDoc comments

--- classes/student.php
+++ classes/student.php
@@ -1,24 +1,42 @@
 <?php
 
+/**
+ * Value object class that holds the information for a single student
+ * Students are compared by score
+ */
 class Student {
 
-    public $id;
+    // ----------------------------------------
+    // PUBLIC CLASS MEMBERS
+    // ----------------------------------------
+    public $id; /*database id*/
     public $name;
     public $facultyNumber;
     public $email;
-    public $score = 0;
+    public $score;
 
+    // ----------------------------------------
+    // CONSTRUCTOR
+    // ----------------------------------------
     public function __construct($id = -1, $name = "", $fn = NULL, $email = NULL) {
         $this->id = $id;
         $this->name = $name;
         $this->facultyNumber = $fn;
         $this->email = $email;
+        $this->score = 0;
     }
+
+    // ----------------------------------------
+    // PUBLIC API METHODS
+    // ----------------------------------------
 
     public function setScore($score) {
         $this->score = $score;
     }
 
+    // ----------------------------------------
+    // STATIC METHODS
+    // ----------------------------------------
     static function compare($a, $b) {
         if ($a->score == $b->score) {
             return 0;
