18a6732370d23ec4d944d5a24f4ebd5b939a280a
Radoslav Georgiev <g2h.horde@gmail.com>
2011-04-25 19:23:56 +0300

    Few changes to make ExtJS work with Smarty template

--- javascript/main.js
+++ javascript/main.js
@@ -1,52 +1,164 @@
-$(document).ready(function() {
-    $("#hidden").hide();
-    $("#submitScore").click(function(){
-	
-        // maybe some validation in the future
-        var name = $("#nameInput").val().trim();
-        var type = $("#scoreTypeInput").val().trim();
+     Ext.BLANK_IMAGE_URL = 'extjs/resources/themes/images/default/tree/s.gif';
+            Ext.require([
+                'Ext.form.*',
+                'Ext.data.*'
+            ]);
 
-        var lecture = $("#lectureNumber").val();
-        var postObject = {
-            "name" : name,
-            "type" : type,
-            "lecture" : lecture
-        };
-        console.log(postObject);
+            Ext.onReady(function(){
+                /********
+                 *MODELS*
+                 ********/
+                Ext.regModel('Students', {
+                    fields : [
+                        {name : "name", type : "string"},
+                        {name : "id", type : "integer"}
+                    ]
+                });
 
-        $.post(Leaderboard.addScriptUrl,
-            postObject,
-            function(response) {
-                console.log("Response from script : ", response);
-                $("#nameInput").val("");
-                $("#scoreTypeInput").val("");
-                //$("#lectureNumber").val("");
-                $("#hidden").show("slow").hide("slow");
-            },
-            "json"
-            );
-    });
+                Ext.regModel('Types', {
+                    fields : [
+                        {name : "typeName", type : "string"}
+                    ]
+                });
 
-    $("#submitHomework").click(function(){
-        var names = $("#csvHomework").val().split(",").map(function(el) {
-            return el.trim();
-        });
-        var homeworkNumber = $("#homeworkNumber").val();
-        console.log(names);
+                /********
+                 *STORES*
+                 ********/
+                Leaderboard.typesStore = new Ext.create("Ext.data.Store", {
+                    model : "Types",
+                    data : [{typeName : "question"}, {typeName : "answer"}, {typeName : "homework"}]
+                });
 
-        var postObject = {
-            "names" : names,
-            "type" : "homework",
-            "lecture" : homeworkNumber
-        };
+                /***********
+                 *FUNCTIONS*
+                 ***********/
+                Leaderboard.ajaxCall = function(parameters, succsess_callback, failure_callback) {
+                    Ext.Ajax.request({
+                        url : Leaderboard.gateway,
+                        success : succsess_callback,
+                        failure : failure_callback,
+                        params : parameters
+                    });
+                };
 
-        $.post(Leaderboard.addScriptUrl,
-            postObject,
-            function(response) {
-                console.log("Response from script : ", response);
-            },
-            "json"
-            );
+                /***********
+                 *APP LOGIC*
+                 ***********/
+                var students = [];
 
-    });
-});
+                var loadSucc = function(response) {
+                    students = Ext.decode(response.responseText);
+                    console.log(students);
+                    Leaderboard.studentsModel = new Ext.create("Ext.data.Store", {
+                        model : "Students",
+                        data : students
+                    });
+                    // cant deal with the fuckin stores for now ...
+                    afterLoad();
+                }
+
+                Leaderboard.ajaxCall({method : "getAllStudents"}, loadSucc);
+
+                var afterLoad = function() {
+                    Leaderboard.addPointsForLecture = Ext.create('Ext.form.FormPanel', {
+                        frame: true,
+                        title: 'Points for lecture',
+                        width: 350,
+                        bodyPadding: 5,
+                        renderTo : "panelsPlaceholder",
+                        collapsible : true,
+                        fieldDefaults: {
+                            labelAlign: 'left',
+                            labelWidth: 90,
+                            anchor: '100%'
+                        },
+
+                        items: [{
+                                id : 'autocompNames',
+                                store : Leaderboard.studentsModel,
+                                xtype: 'combo',
+                                name: 'nameInput',
+                                fieldLabel: 'Name',
+                                queryMode: 'local',
+                                displayField: 'name',
+                                valueField : 'id'
+                            },{
+                                id : 'pointsInput',
+                                xtype: 'numberfield',
+                                name: 'pointsInput',
+                                fieldLabel: 'Points',
+                                value: 0.5,
+                                step : 0.1,
+                                minValue: 0,
+                                maxValue: 50
+                            }, {
+                                id : 'typeInput',
+                                xtype : 'combo',
+                                fieldLabel : 'type',
+                                store : Leaderboard.typesStore,
+                                queryMode : 'local',
+                                displayField : 'typeName',
+                                valueField : 'typeName'
+                            },
+                            {
+                                id : 'lectureInput',
+                                xtype : 'numberfield',
+                                name : 'lectureInput',
+                                fieldLabel : 'Lecture',
+                                value : 1,
+                                step : 1,
+                                minValue : 1,
+                                maxValue : 20
+                            }],
+                        buttons : [
+                            {
+                                text : 'Add points',
+                                handler : function() {
+                                    var cb = Ext.getCmp("autocompNames");
+
+                                    var studentName = cb.rawValue;
+                                    var studentId = cb.value;
+
+                                    /*mark as new student to the database*/
+                                    if(studentId === null) {
+                                        studentId = -1;
+                                    }
+
+                                    var points = Ext.getCmp("pointsInput").value;
+                                    var type = Ext.getCmp("typeInput").value;
+                                    var lecture = Ext.getCmp("lectureInput").value;
+
+                                    var params = {
+                                        "method" : "addPoints",
+                                        "studentName" : studentName,
+                                        "studentId" : studentId,
+                                        "points" : points,
+                                        "lecture" : lecture,
+                                        "type" : type
+                                    };
+
+                                    console.log(params);
+                                    Leaderboard.ajaxCall(params, function(resp) {
+                                        //console.log("response from adding : ", resp);
+                                        var respObj = Ext.decode(resp.responseText);
+
+                                        if(typeof(respObj["studentId"]) !== "undefined") {
+                                            console.log("newly added");
+                                            console.log(respObj);
+                                            studentId = respObj["studentId"];
+                                            console.log(studentId);
+                                            Ext.getCmp("autocompNames").store.add({name : studentName, id : studentId});
+                                        }
+
+                                        Ext.get("scoreAnim").show(true).on("afteranimate", function() {
+                                            Ext.get("scoreAnim").show({duration : 1.5 /*seconds*/});
+                                        });
+
+                                    });
+                                }
+                            }
+                        ]
+                    });
+                }
+
+            });
---
+++ templates/adminPage.tpl
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <!-- INCLUDE EXT JS -->
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+        <!-- ** CSS ** -->
+        <!-- base library -->
+        <link rel="stylesheet" type="text/css" href="javascript/extjs/resources/css/ext-all.css" />
+
+        <!-- ** Javascript ** -->
+        <script src="javascript/namespace.js" type="text/javascript" language="javascript" charset="utf-8"></script>
+        <!-- ExtJS library: base/adapter -->
+        <script type="text/javascript" src="javascript/extjs/ext-all-debug-w-comments.js"></script>
+
+        <!-- page specific -->
+        <script type="text/javascript" src="javascript/main.js"></script>
+        <title>Add points</title>
+
+
+    </head>
+
+    <body>
+        <div id="panelsPlaceholder"></div>
+        <div style="font-weight: bold; color: green; visibility: hidden" id="scoreAnim">Score added</div>
+    </body>
+</html>
