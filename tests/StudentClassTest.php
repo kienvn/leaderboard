0701787c71b58c006e0a06a5ec1992efef080700
Radoslav Georgiev <g2h.horde@gmail.com>
2011-05-07 18:04:33 +0300

    Added test case for testing the static compare method of Student class

--- tests/StudentClassTest.php
+++ tests/StudentClassTest.php
@@ -9,6 +9,7 @@ require_once("php_unit_config.php");
 require_once("../classes/student.php");
 
 class StudentClassTest extends PHPUnit_Framework_TestCase {
+
     public function testEmptyStudentObject() {
         $student = new Student();
 
@@ -25,7 +26,7 @@ class StudentClassTest extends PHPUnit_Framework_TestCase {
         $email = "student@uni.com";
         $name = "Student Student";
         $score = 15;
-        
+
         $student = new Student($id, $name, $fn, $email);
         $student->setScore($score);
 
@@ -35,4 +36,21 @@ class StudentClassTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals($student->score, $score);
         $this->assertEquals($student->name, $name);
     }
+
+    public function testStaticMethodForComparingStudentObjects() {
+        $s1 = new Student();
+        $s1->setScore(10);
+
+        $s2 = new Student();
+        $s2->setScore(15);
+
+        $this->assertEquals(Student::compare($s1, $s2), 1);
+
+        $s1->setScore(20);
+        $this->assertEquals(Student::compare($s1, $s2), -1);
+
+        $s1->setScore(15);
+        $this->assertEquals(Student::compare($s1, $s2), 0);
+    }
+
 }
