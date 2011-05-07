7261be6ad411e5e7ec172665d66d31cff9f0f22e
Radoslav Georgiev <g2h.horde@gmail.com>
2011-05-07 16:54:19 +0300

    First commit for the unit tests for the project.

---
+++ tests/StudentFactoryTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * PHPUnit test for the StudentFactory class
+ *
+ * @author RadoRado (a.k.a Rado)
+ */
+require_once("php_unit_config.php");
+require_once("config/testDatabase_config.php");
+
+require_once("../classes/config.php");
+require_once("../classes/databaseaware.php");
+require_once("../classes/database.php");
+require_once("../classes/studentfactory.php");
+
+class StudentFactoryTest extends PHPUnit_Framework_TestCase {
+
+    public static $database;
+    protected $studentFactory;
+
+    public static function setUpBeforeClass() {
+        global $dbConfig;
+        self::$database = new Database($dbConfig, true);
+    }
+
+    public static function tearDownAfterClass() {
+        global $dbConfig;
+        self::$database->query("DROP DATABASE " . $dbConfig["DB_NAME"]);
+    }
+
+    protected function setUp() {
+        // todo
+    }
+
+    public function testSomething() {
+        $this->assertEquals(TRUE, TRUE);
+    }
+
+}
---
+++ tests/php_unit_config.php
@@ -0,0 +1,5 @@
+<?php
+require_once("PHPUnit/Framework/Assert.php"); /*PHPUnit_Framework_Assert class*/
+require_once("PHPUnit/Framework/Test.php"); /*PHPUnit_Framework_Test interface*/
+require_once("PHPUnit/Framework/SelfDescribing.php"); /*PHPUnit_Framework_SelfDescribing interface*/
+require_once("PHPUnit/Framework/TestCase.php"); /*PHPUnit_Framework_TestCase class*/
